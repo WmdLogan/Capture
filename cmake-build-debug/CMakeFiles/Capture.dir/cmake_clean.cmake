6f6c5ef67c97c3e1a0be3c0aa6d8dda65cc8dd66
WmdLogan <710222440@qq.com>
2020-04-19 15:10:41 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/Capture.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Capture.dir/main.c.obj"
+  "Capture.exe"
+  "Capture.exe.manifest"
+  "Capture.pdb"
+  "libCapture.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Capture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
